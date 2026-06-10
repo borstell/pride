67ced481ea135ae3bc15f41a975d208b3d9afce9
Carl Börstell <15570645+borstell@users.noreply.github.com>
2026-06-10 12:13:38 +0200

    Add files via upload

--- pride_flag.R
+++ pride_flag.R
@@ -1,4 +1,3 @@
-
 # Load packages -----------------------------------------------------------
 
 library(ggplot2)
@@ -7,18 +6,14 @@ library(ggplot2)
 
 # Define colors -----------------------------------------------------------
 
-r <- "#CF3829"
-o <- "#E48E39"
-y <- "#EDE553"
-g <- "#86B644"
-bl <- "#37579F"
-pu <- "#64287C"
+rainbow <- c("#64287C", "#37579F", "#86B644", "#EDE553", "#E48E39", "#CF3829")
 k <- "#000000"
 br <- "#8B5826"
-bb <- "#8ECAE2"
-p <- "#E9B1C7"
-w <- "#FFFFFF"
-
+bl <- "#8ECAE2"
+pi <- "#E9B1C7"
+wh <- "#FFFFFF"
+yl <- "#F7D94D"
+pu <- "#5F3687"
 
 
 # Set flag ratio ----------------------------------------------------------
@@ -32,39 +27,55 @@ my_ratio <- 5/8
 
 # Pride Flag
 
-pride <- 
-  ggplot() +
-  annotate("rect", xmin = 0, xmax = 6, ymin = 0:5, ymax = 1:6, fill = I(c(pu, bl, g, y, o, r))) +
-  #annotate("rect", xmin = 0, xmax = 6, ymin = 0, ymax = 6, fill = NA, color = "grey40") +
+ggplot() +
+  annotate("rect", xmin = 0, xmax = 6, ymin = 0:5, ymax = 1:6, fill = I(rainbow)) +
+  annotate("rect", xmin = 0, xmax = 6, ymin = 0, ymax = 6, fill = NA, color = "white") +
   coord_equal(ratio = my_ratio) +
   theme_void()
 
-pride
-
-
-# Progressive Pride Flag
-progressive <- 
-  ggplot() +
-  annotate("rect", xmin = 0, xmax = 6, ymin = 0:5, ymax = 1:6, fill = I(c(pu, bl, g, y, o, r))) +
-  annotate("polygon", x = c(0, 0, .9, 2.7, .9, 0), y = c(0, 6, 6, 3, 0, 0), alpha = 1, fill = k) +
-  annotate("polygon", x = c(0, 0, .45, 2.25, .45, 0), y = c(0, 6, 6, 3, 0, 0), alpha = 1, fill = br) +
-  annotate("polygon", x = c(0, 0, 1.8, 0), y = c(0, 6, 3, 0), alpha = 1, fill = bb) +
-  annotate("polygon", x = c(0, 0, 1.35, 0), y = c(.75, 5.25, 3, .75), alpha = 1, fill = p) +
-  annotate("polygon", x = c(0, 0, .9, 0), y = c(1.5, 4.5, 3, 1.5), alpha = 1, fill = w) +
-  #annotate("rect", xmin = 0, xmax = 6, ymin = 0, ymax = 6, fill = NA, color = "grey40") +
-  coord_equal(ratio = my_ratio) +
-  theme_void()
-
-progressive
 
 
 # Trans Flag
-trans <- 
-  ggplot() +
-  annotate("rect", xmin = 0, xmax = 5, ymin = 0:4, ymax = 1:5, fill = I(c(bb, p, w, p, bb))) +
-  #annotate("rect", xmin = 0, xmax = 5, ymin = 0, ymax = 5, fill = NA, color = "grey40") +
+ggplot() +
+  annotate("rect", xmin = 0, xmax = 5, ymin = 0:4, ymax = 1:5, fill = I(c(bl, pi, wh, pi, bl))) +
+  annotate("rect", xmin = 0, xmax = 5, ymin = 0, ymax = 5, fill = NA, color = "white") +
   coord_equal(ratio = my_ratio) +
   theme_void()
 
-trans
+
+
+# Progressive Pride Flag
+ggplot() +
+  annotate("rect", xmin = 0, xmax = 6, ymin = 0:5, ymax = 1:6, fill = I(rainbow)) +
+  annotate("polygon", x = c(0, 0, .9, 2.7, .9, 0), y = c(0, 6, 6, 3, 0, 0), alpha = 1, fill = k) +
+  annotate("polygon", x = c(0, 0, .45, 2.25, .45, 0), y = c(0, 6, 6, 3, 0, 0), alpha = 1, fill = br) +
+  annotate("polygon", x = c(0, 0, 1.8, 0), y = c(0, 6, 3, 0), alpha = 1, fill = bl) +
+  annotate("polygon", x = c(0, 0, 1.35, 0), y = c(.75, 5.25, 3, .75), alpha = 1, fill = pi) +
+  annotate("polygon", x = c(0, 0, .9, 0), y = c(1.5, 4.5, 3, 1.5), alpha = 1, fill = wh) +
+  annotate("rect", xmin = 0, xmax = 6, ymin = 0, ymax = 6, fill = NA, color = "white") +
+  coord_equal(ratio = my_ratio) +
+  theme_void()
+
+
+
+# Progressive Inclusive Pride Flag
+ggplot() +
+  annotate("rect", xmin = 0, xmax = 6, ymin = 0:5, ymax = 1:6, fill = I(rainbow)) +
+  annotate("polygon", x = c(0, 0, 1.7, 3.4, 1.7, 0), y = c(0, 6, 6, 3, 0, 0), 
+           alpha = 1, fill = k) +
+  annotate("polygon", x = c(0, 0, 1.3, 3, 1.3, 0), y = c(0, 6, 6, 3, 0, 0), 
+           alpha = 1, fill = br) +
+  annotate("polygon", x = c(0, 0, .9, 2.6, .9, 0), y = c(0, 6, 6, 3, 0, 0), 
+           alpha = 1, fill = bl) +
+  annotate("polygon", x = c(0, 0, .5, 2.2, .5, 0), y = c(0, 6, 6, 3, 0, 0),
+           alpha = 1, fill = pi) +
+  annotate("polygon", x = c(0, 0, .1, 1.8, .1, 0), y = c(0, 6, 6, 3, 0, 0),
+           alpha = 1, fill = wh) +
+  annotate("polygon", x = c(0, 0, 1.4, 0), y = c(1.5, 5.475, 3, .525), 
+           alpha = 1, fill = yl) +
+  annotate("point", x = .55, y = 3, shape = 21, size = 22, stroke = 4, color = pu) +
+  annotate("rect", xmin = 0, xmax = 6, ymin = 0, ymax = 6, 
+           fill = NA, color = "white") +
+  coord_equal(ratio = 5/8) +
+  theme_void()
 
